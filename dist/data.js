d1f62169b5923df0d4a115e574dceb01caa1a702
Jesse Hixson <hixsonj@gmail.com>
2015-05-28 10:20:07 -0400

    Adds dist for github pages.

---
+++ dist/data.js
@@ -0,0 +1,114 @@
+var data = [
+  {
+    id: 1,
+    title: 'Lost in the Dream',
+    artist: 'The War on Drugs',
+    cover: 'war_on_drugs.jpg',
+    preview: 'http://a641.phobos.apple.com/us/r1000/011/Music4/v4/21/f9/93/21f99363-22d7-f648-933e-09df8aeebb4b/mzaf_5879262719553160122.plus.aac.p.m4a'
+  },
+  {
+    id: 2,
+    title: 'This Is All Yours',
+    artist: 'Alt-J',
+    cover: 'alt-j.jpg',
+    preview: 'http://a1572.phobos.apple.com/us/r1000/151/Music3/v4/94/e9/50/94e95060-7dc9-7e50-7564-2a4333ce0133/mzaf_2405467462712005286.plus.aac.p.m4a'
+  },
+  {
+    id: 3,
+    title: 'Run the Jewels 2',
+    artist: 'Run the Jewels',
+    cover: 'run_the_jewels.jpg',
+    preview: 'http://a891.phobos.apple.com/us/r1000/160/Music1/v4/d8/71/92/d871924a-4cda-66e1-ecce-5c66e370c201/mzaf_420885157497495184.plus.aac.p.m4a'
+  },
+  {
+    id: 4,
+    title: 'The Voyager',
+    artist: 'Jenny Lewis',
+    cover: 'jenny_lewis.jpg',
+    preview: 'http://a244.phobos.apple.com/us/r1000/019/Music4/v4/82/34/76/8234761d-8e0a-097f-3a0b-dc1632c19750/mzaf_5556074102589787905.plus.aac.p.m4a'
+  },
+  {
+    id: 5,
+    title: 'Stay Gold',
+    artist: 'First Aid Kit',
+    cover: 'first_aid_kit.jpg',
+    preview: 'http://a1866.phobos.apple.com/us/r1000/051/Music4/v4/2e/a2/72/2ea272b5-07ad-4460-2d65-5b13124e0a4b/mzaf_6581513488396341852.plus.aac.p.m4a'
+  },
+  {
+    id: 6,
+    title: 'ZABA',
+    artist: 'Glass Animals',
+    cover: 'glass_animals.jpg',
+    preview: 'http://a1649.phobos.apple.com/us/r1000/018/Music/v4/b6/c4/86/b6c486c7-84b5-0dd5-5ada-25e57160cc92/mzaf_3889615811898581941.plus.aac.p.m4a'
+  },
+  {
+    id: 7,
+    title: 'HEAL',
+    artist: 'Strand of Oaks',
+    cover: 'strand_of_oaks.jpg',
+    preview: 'http://a860.phobos.apple.com/us/r1000/004/Music2/v4/5c/22/2d/5c222d5a-e455-de54-48a3-e0090cd28521/mzaf_7205187005036684242.plus.aac.p.m4a'
+  },
+  {
+    id: 8,
+    title: '',
+    artist: 'Sylvan Esso',
+    cover: 'sylvan_esso.jpg',
+    preview: 'http://a1831.phobos.apple.com/us/r1000/021/Music6/v4/d7/a3/a3/d7a3a3e3-869c-d695-c260-f9d50f6532a8/mzaf_458007514101652236.plus.aac.p.m4a'
+  },
+  {
+    id: 9,
+    title: '1989',
+    artist: 'Taylor Swift',
+    cover: 'taylor_swift.jpg',
+    preview: 'http://a1007.phobos.apple.com/us/r1000/145/Music3/v4/11/cf/71/11cf71d7-5a2b-660c-eef2-a8dacd9694cb/mzaf_9148830979689986253.plus.aac.p.m4a'
+  },
+  {
+    id: 10,
+    title: "It's Album Time",
+    artist: 'Todd Terje',
+    cover: 'todd_terje.jpg',
+    preview: 'http://a1469.phobos.apple.com/us/r1000/053/Music4/v4/56/4e/50/564e5056-4c45-480d-5c5c-394b9cb0c868/mzaf_2002661515504000848.plus.aac.p.m4a'
+  },
+  {
+    id: 11,
+    title: 'Never Hungover Again',
+    artist: 'Joyce Manor',
+    cover: 'joyce_manor.jpg',
+    preview: 'http://a1355.phobos.apple.com/us/r1000/051/Music/v4/6c/2a/cc/6c2acc3a-7161-62c2-64bc-70b61971bc5a/mzaf_7392195988824203114.plus.aac.p.m4a'
+  },
+  {
+    id: 12,
+    title: 'Shakey Graves',
+    artist: 'And the War Came',
+    cover: 'shakey_graves.jpg',
+    preview: 'http://a1954.phobos.apple.com/us/r30/Music3/v4/5f/3a/18/5f3a1852-6e87-1564-0a1e-2238a649b91f/mzaf_5268507797390650624.plus.aac.p.m4a'
+  },
+  {
+    id: 13,
+    title: 'Smoke & Mirrors',
+    artist: 'Ivy and Gold',
+    cover: 'ivy_and_gold.jpg',
+    preview: 'http://a922.phobos.apple.com/us/r30/Music3/v4/2e/4c/e5/2e4ce502-bc57-df11-c4b9-0d8990984a77/mzaf_7101523954946332364.plus.aac.p.m4a'
+  },
+  {
+    id: 14,
+    title: 'Little Machines',
+    artist: 'Lights',
+    cover: 'lights.jpg',
+    preview: 'http://a1148.phobos.apple.com/us/r1000/035/Music4/v4/a3/7c/aa/a37caa69-a560-e7a2-7afc-15c42fc6cc09/mzaf_7415199458591492588.plus.aac.p.m4a'
+  },
+  {
+    id: 15,
+    title: 'Before the Waves',
+    artist: 'Magic Man',
+    cover: 'magic_man.jpg',
+    preview: 'http://a1298.phobos.apple.com/us/r1000/043/Music4/v4/fa/e0/06/fae00640-af4d-acd2-8a69-7733bbaae884/mzaf_3176427827653476227.plus.aac.p.m4a'
+  },
+  {
+    id: 16,
+    title: 'Talking is Hard',
+    artist: 'Walk the Moon',
+    cover: 'walk_the_moon.jpg',
+    preview: 'http://a1238.phobos.apple.com/us/r1000/019/Music1/v4/b4/3c/c4/b43cc400-ef3b-2656-5838-27616ebbc939/mzaf_6009417605643401308.plus.aac.p.m4a'
+  }
+];
